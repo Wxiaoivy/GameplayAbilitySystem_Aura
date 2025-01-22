68b2a80b6897af8a27c63931b4d2f62ef2777b50
Ivy <331998422@qq.com>
2025-01-22 20:31:26 +0800

    test

--- Source/Aura/Private/Player/MyPlayerController.cpp
+++ Source/Aura/Private/Player/MyPlayerController.cpp
@@ -3,3 +3,22 @@
 
 #include "Player/MyPlayerController.h"
 
+AMyPlayerController::AMyPlayerController()
+{
+	bReplicates = true;
+
+}
+
+void AMyPlayerController::BeginPlay()
+
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+    //UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem< UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+}
+
+void AMyPlayerController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+}
--- Source/Aura/Public/Player/MyPlayerController.h
+++ Source/Aura/Public/Player/MyPlayerController.h
@@ -3,15 +3,40 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/PlayerController.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacter.h"
+//#include "EnhancedInputModule.h"
+//#include "InputActionValue.h"
+//#include "EnhancedInputComponent.h"
+//#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Controller.h"
 #include "MyPlayerController.generated.h"
+
 
 /**
  * 
  */
+class UInputMappingContext;
+
+
 UCLASS()
 class AURA_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AMyPlayerController();
+	
+protected:
+	virtual void  BeginPlay()override;
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
+
+
+private:
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext>AuraContext;
+
 	
 };
