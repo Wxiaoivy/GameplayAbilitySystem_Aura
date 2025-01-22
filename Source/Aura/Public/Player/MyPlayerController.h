94ffc176dd6082aefa2a70ab4b8376d4c708a09f
Ivy <331998422@qq.com>
2025-01-22 16:53:06 +0800

    8/200(一)

---
+++ Source/Aura/Public/Player/MyPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Ivy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
